a92e19b887fd5e7578651682d6c69b7ddddc3da0
Mark Carey <796+kogent@users.noreply.github.com>
2018-08-08 21:45:23 -0700

    trigger vim plugin installs

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -3,6 +3,11 @@ if [[ ! -x "$(which stow)" ]]; then
     echo "please install gnu stow to install dotfiles"
     exit 1
 fi
+if [[ ! -x "$(which git)" ]]; then
+    echo "please install git to install dotfiles"
+    exit 1
+fi
+
 stow --target=${HOME} bash
 stow --target=${HOME} misc
 stow --target=${HOME} python
@@ -10,6 +15,10 @@ stow --target=${HOME} ruby
 stow --target=${HOME} shell_common
 stow --target=${HOME} vim
 stow --target=${HOME} zsh
+
+# vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 
 # symlink ~/.local to ./local
 if [[ ! -L "./local" ]]; then
